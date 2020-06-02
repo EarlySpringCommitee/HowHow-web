990bd8fae59c1b2224fd14fc1202117086e3d18e
gnehs <jayuiop@gmail.com>
2020-06-02 13:28:24 +0800

    語音會動了

---
+++ assets/js/how-speak.js
@@ -0,0 +1,62 @@
+const ap = new APlayer({ container: document.getElementById('aplayer') });
+let voiceBaseURL = "/assets/audios/"
+let voiceList = {}
+let failVoice = voiceBaseURL + '沒有這個音.mp3'
+async function fetchVoiceList() {
+    if (sessionStorage['audio']) {
+        voiceList = JSON.parse(sessionStorage['audio'])
+    }
+    else {
+        let res = await fetch('https://api.github.com/repos/EarlySpringCommitee/HowHow-web/contents/assets/audios?ref=master').then(x => x.json())
+        for (let audio of res) {
+            let { name } = audio
+            voiceList[name.replace('.mp3', '')] = voiceBaseURL + name
+        }
+        sessionStorage['audio'] = JSON.stringify(voiceList)
+    }
+    $("#play").removeAttr("disabled")
+
+}
+async function chinses2Pinyin(text) {
+    // 繁化姬
+    /*return (await fetch("https://api.zhconvert.org/convert", {
+         method: 'POST',
+         body: JSON.stringify({
+             converter: "Pinyin",
+             text
+         }),
+         headers: new Headers({
+             'Content-Type': 'application/json'
+         })
+     }).then(x => x.json())).data.text.split(' ')*/
+    // 路邊撿的
+    let helloacm = (await fetch(`https://helloacm.com/api/pinyin/?cached&s=${text}&t=1`).then(x => x.json())).result
+
+    return helloacm.map(x => x.indexOf(',') > 0 ? x.split(',')[0] : x)
+}
+async function speak(text) {
+    let pinyin = await chinses2Pinyin(text)
+    ap.list.clear()
+    for (let s of pinyin) {
+        if (voiceList[s]) {
+            ap.list.add([{
+                name: s,
+                url: voiceList[s]
+            }]);
+        } else {
+            console.warn(`沒有這個音: ${s}`)
+            ap.list.add([{
+                name: s,
+                url: failVoice
+            }]);
+        }
+    }
+    ap.play()
+}
+$("#play").click(function () {
+    speak($("#how-text").val())
+})
+ap.on('ended', function () {
+    ap.list.remove(0);
+});
+fetchVoiceList()
