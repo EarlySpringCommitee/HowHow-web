fb29e6fe40807ec5a390842515a4b7f5d2851690
gnehs <jayuiop@gmail.com>
2020-06-06 21:56:19 +0800

    嘗試使用 MediaSource 混合影片、新增 Webm 檔案

--- assets/js/how-speak.js
+++ assets/js/how-speak.js
@@ -84,18 +84,19 @@ async function speakVideo(text) {
     let pinyin = await chinses2Pinyin(text)
     _how_vlist = []
     _how_vlist_active = 0
-    let videoLoader = v => {
-        let headID = document.getElementsByTagName('head')[0];
-        let link = document.createElement('link');
-        link.as = 'video';
-        link.rel = 'preload'
-        link.href = v
-        headID.appendChild(link);
-    };
-
     for (let s of pinyin) {
         let pushVideo = v => {
-            videoLoader(v)
+            if (!'MediaSource' in window) {
+                let videoLoader = v => {
+                    let headID = document.getElementsByTagName('head')[0];
+                    let link = document.createElement('link');
+                    link.as = 'video';
+                    link.rel = 'preload'
+                    link.href = v
+                    headID.appendChild(link);
+                };
+                videoLoader(v)
+            }
             _how_vlist.push({
                 v,
                 played: false
@@ -114,20 +115,96 @@ async function speakVideo(text) {
 
         }
     }
-    howVideoEl.src = _how_vlist[0].v
-    howVideoEl.play();
-    howVideoEl.addEventListener('playing', function (e) {
-        _how_vlist[_how_vlist_active].played = true
-    })
-    howVideoEl.addEventListener('ended', function (e) {
-        if (_how_vlist[_how_vlist_active] && _how_vlist[_how_vlist_active].played) {
-            _how_vlist_active++
-            if (_how_vlist.length > _how_vlist_active) {
-                howVideoEl.src = _how_vlist[_how_vlist_active].v;
-                howVideoEl.play();
+    if (!'MediaSource' in window) {
+        howVideoEl.src = _how_vlist[0].v
+        howVideoEl.play();
+        howVideoEl.addEventListener('playing', function (e) {
+            _how_vlist[_how_vlist_active].played = true
+        })
+        howVideoEl.addEventListener('ended', function (e) {
+            if (_how_vlist[_how_vlist_active] && _how_vlist[_how_vlist_active].played) {
+                _how_vlist_active++
+                if (_how_vlist.length > _how_vlist_active) {
+                    howVideoEl.src = _how_vlist[_how_vlist_active].v;
+                    howVideoEl.play();
+                }
             }
-        }
-    });
+        });
+    } else {
+        (async () => {
+
+
+            const mediaSource = new MediaSource();
+
+            const video = howVideoEl
+
+            // video.oncanplay = e => video.play();
+
+            const urls = JSON.parse(JSON.stringify(_how_vlist)).map(x => x.v).map(x => x.replace('/videos', '/webm').replace('mp4', 'webm'))
+
+            const request = url => fetch(url).then(response => response.arrayBuffer());
+
+            // `urls.reverse()` stops at `.currentTime` : `9`
+            const files = await Promise.all(urls.map(request));
+
+            /*
+             `.webm` files
+             Uncaught DOMException: Failed to execute 'appendBuffer' on 'SourceBuffer': This SourceBuffer has been removed from the parent media source.
+             Uncaught DOMException: Failed to set the 'timestampOffset' property on 'SourceBuffer': This SourceBuffer has been removed from the parent media source.
+            */
+            // const mimeCodec = "video/webm; codecs=opus";
+            // https://stackoverflow.com/questions/14108536/how-do-i-append-two-video-files-data-to-a-source-buffer-using-media-source-api/
+            const mimeCodec = "video/mp4; codecs=avc1.64001F, mp4a.40.2";
+
+
+            const media = await Promise.all(files.map(file => {
+                return new Promise(resolve => {
+                    let media = document.createElement("video");
+                    let blobURL = URL.createObjectURL(new Blob([file]));
+                    media.onloadedmetadata = async e => {
+                        resolve({
+                            mediaDuration: media.duration,
+                            mediaBuffer: file
+                        })
+                    }
+                    media.src = blobURL;
+                })
+            }));
+
+            console.log(media);
+
+            mediaSource.addEventListener("sourceopen", sourceOpen);
+
+            video.src = URL.createObjectURL(mediaSource);
+
+            async function sourceOpen(event) {
+
+                if (MediaSource.isTypeSupported(mimeCodec)) {
+                    const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+
+                    for (let chunk of media) {
+                        await new Promise(resolve => {
+                            sourceBuffer.appendBuffer(chunk.mediaBuffer);
+                            sourceBuffer.onupdateend = e => {
+                                sourceBuffer.onupdateend = null;
+                                sourceBuffer.timestampOffset += chunk.mediaDuration;
+                                console.log(mediaSource.duration);
+                                resolve()
+                            }
+                        })
+
+                    }
+
+                    mediaSource.endOfStream();
+
+                }
+                else {
+                    console.warn(mimeCodec + " not supported");
+                }
+            };
+
+        })()
+    }
 }
 $("#play").click(function () {
     speak($("#how-text").val())
